4deea4be975581c5f838fc5d76b04488e857c6f0
Guide Fari <guideg6@gmail.com>
2022-07-27 06:56:26 +0200

    gotcha bitch!
    I hope :laugh:

--- pages/genres.js
+++ pages/genres.js
@@ -20,12 +20,12 @@ export default function Genres({ tags }) {
         description="Guess what, more things to do with curated music"
       />
       <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
-        <div className="space-x-2 pt-6 pb-8 md:space-y-5">
+        <div className="pt-6 pb-8 space-x-2 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:border-r-2 md:px-6 md:text-6xl md:leading-14">
-            Genres
+            Genres <br /> & tags
           </h1>
         </div>
-        <div className="flex max-w-lg flex-wrap">
+        <div className="flex flex-wrap max-w-lg">
           {Object.keys(tags).length === 0 && 'No tags found.'}
           {sortedTags.map((t) => {
             return (
@@ -33,7 +33,7 @@ export default function Genres({ tags }) {
                 <Tag text={t} />
                 <Link
                   href={`/tags/${kebabCase(t)}`}
-                  className="-ml-2 text-sm font-semibold uppercase text-gray-600 dark:text-gray-300"
+                  className="-ml-2 text-sm font-semibold text-gray-600 uppercase dark:text-gray-300"
                 >
                   {` (${tags[t]})`}
                 </Link>
