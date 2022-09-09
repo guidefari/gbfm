0bdd1fa454f4201ca23fcdea436e5fcd74f2e112
Guide Fari <guideg6@gmail.com>
2022-09-09 08:58:35 +0200

    okay super small change

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export default function Index({ posts }) {
 
   return (
     <Layout>
-      <PageSEO title="Goosebumps dot fm" description={'Curated Music & the occasional prose'} />
+      <PageSEO title="goosebumps dot fm" description={'Curated Music & the occasional prose'} />
       <SuperHero />
       <h3 className="title">Prose & Sounds</h3>
       <section className="grid grid-cols-1 gap-12 mx-4 lg:mx-auto max-w-7xl lg:gap-24 lg:grid-cols-2">
