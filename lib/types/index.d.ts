7868b6b86764650281938770dda949aa9e783237
Guide Fari <guideg6@gmail.com>
2023-02-01 23:12:08 +0200

    types

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -16,3 +16,38 @@ export type Tweet = {
   date: string
   children: React.ReactNode
 }
+
+export type TrackAPIResponse = {
+  albumType?: string
+  albumImageUrl?: string
+  title?: string
+  artists?: string
+  trackUrl?: string
+  previewUrl?: string
+}
+
+export type GenericAndMaybeLegacyError = {
+  error: string
+}
+
+export interface PlaylistApiResponse {
+  coverImageUrl: string
+  title: string
+  description: string
+  tracks: TrackAPIResponse[]
+  ownerName: string
+  playlistUrl: string
+}
+
+export type AlbumSingleTrackApiResponse = TrackAPIResponse<Omit, 'album_type' | 'albumImageUrl'>
+
+export type AlbumApiResponse = {
+  albumType?: string
+  albumImageUrl?: string
+  title?: string
+  artists?: string
+  trackUrl?: string
+  previewUrl?: string
+  tracks: AlbumSingleTrackApiResponse[]
+  albumUrl: string
+}
