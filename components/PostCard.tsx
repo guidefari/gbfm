06156ab5e3744dbc29434eb5edda635604aef8a3
Guide Fari <guideg6@gmail.com>
2023-03-21 19:15:26 +0200

    feat: clickable image on curated site

--- components/PostCard.tsx
+++ components/PostCard.tsx
@@ -14,18 +14,20 @@ interface Props {
 export const PostCard: React.FC<Props> = ({ date, title, description, thumbnailUrl, slug }) => (
   <div className="sm:flex lg:items-start group">
     <div className="flex-shrink-0 mb-4 sm:mb-0 sm:mr-4">
-      <Image
-        className="object-cover w-full rounded-md sm:h-32 sm:w-32"
-        src={
-          thumbnailUrl ||
-          'https://res.cloudinary.com/hokaspokas/image/upload/v1663215741/goosebumpsfm/generic_Thumb.svg'
-        }
-        alt={`Thumbnail image for post titled - ${title}`}
-        width={320}
-        height={320}
-        loading="lazy"
-        quality={100}
-      />
+      <CustomLink href={slug} as={slug} className="text-xl ">
+        <Image
+          className="object-cover w-full transition ease-in-out rounded-md sm:h-32 sm:w-32 hover:ring-4"
+          src={
+            thumbnailUrl ||
+            'https://res.cloudinary.com/hokaspokas/image/upload/v1663215741/goosebumpsfm/generic_Thumb.svg'
+          }
+          alt={`Thumbnail image for post titled - ${title}`}
+          width={320}
+          height={320}
+          loading="lazy"
+          quality={100}
+        />
+      </CustomLink>
     </div>
     <div>
       <time dateTime={date} className="text-sm opacity-80">
