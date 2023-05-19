e3541abd197a3cb993bcf175b01283ae9dc31037
Guide Fari <guideg6@gmail.com>
2023-05-19 21:09:38 +0200

    lint fix

--- src/lib/fetcher.ts
+++ src/lib/fetcher.ts
@@ -1,8 +1,6 @@
-export default async function fetcher<JSON = any>(
-  input: RequestInfo,
-  init?: RequestInit
-): Promise<unknown> {
+export default async function fetcher<T = any>(input: RequestInfo, init?: RequestInit): Promise<T> {
   const res = await fetch(input, init)
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-return
   return res.json()
 }
