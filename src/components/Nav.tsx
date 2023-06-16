c94b94039e86f552ab77467ae39bb1af3cdea195
Guide Fari <guideg6@gmail.com>
2023-06-16 20:49:51 +0200

    fix: mobile styles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -8,7 +8,7 @@ const SideBar = () => {
   const router = useRouter()
   return (
     <>
-      <nav className="fixed z-50 h-16 max-w-lg -translate-x-1/2 bg-white border border-gray-200 rounded-lg bottom-4 left-1/2 dark:bg-gray-600 dark:border-gray-600">
+      <nav className="fixed z-50 h-16 max-w-lg pr-1 translate-x-1/2 bg-white border border-gray-200 rounded-lg right-1/2 translate bottom-4 dark:bg-gray-600 dark:border-gray-600">
         <div className="grid h-full max-w-lg grid-flow-col mx-auto">
           <button
             onClick={() => router.push('/')}
@@ -53,7 +53,7 @@ const SideBar = () => {
               </button>
               <Image
                 src={thumbnailUrl}
-                className="m-auto mr-1 rounded-md aspect-square w-14"
+                className="min-w-[45px] w-12 m-auto rounded-md aspect-square "
                 alt=""
                 width={80}
                 height={80}
