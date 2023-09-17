4487337644fd966f4216688fa13178d80ed85604
Guide Fari <guideg6@gmail.com>
2023-09-17 08:17:27 +0200

    make download optional

--- src/components/common/MinimalCard.tsx
+++ src/components/common/MinimalCard.tsx
@@ -20,6 +20,7 @@ interface Props {
   previewUrl?: string
   children?: React.ReactNode
   spotify?: boolean
+  download?: boolean
 }
 
 function cn(...classes: string[]) {
@@ -35,10 +36,11 @@ export const MinimalCard: React.FC<Props> = ({
   previewUrl,
   children,
   artists,
+  download = false,
 }) => {
   const [, { handleAlbumArtClick }, isPlaying, nowPlayingImageUrl] = useAudioPlayerContext()
 
-  const iconClassNames = 'text-sky-300 hover:text-gb-tomato'
+  const iconClassNames = 'min-w-[19px] aspect-square text-sky-300 hover:text-gb-tomato'
   function renderIcon(isPlaying: boolean, imageUrl, nowPlayingImageUrl): React.ReactNode {
     if (nowPlayingImageUrl !== imageUrl) return <GiPlayButton className={iconClassNames} />
     if (!isPlaying && nowPlayingImageUrl == imageUrl)
@@ -58,7 +60,7 @@ export const MinimalCard: React.FC<Props> = ({
   }
 
   return (
-    <div className="relative z-10 flex-shrink-0 max-w-md my-8 border-2 border-t-0 border-l-0 rounded-md border-gb-tomato ">
+    <div className="relative z-10 flex-shrink-0 max-w-md my-8 border-2 border-t-0 border-l-0 rounded-md md:max-w-xs border-gb-tomato ">
       <div className="relative flex-shrink-0 mb-4 sm:mb-0 sm:mr-4 group">
         <Image
           className={cn(
@@ -67,8 +69,8 @@ export const MinimalCard: React.FC<Props> = ({
           )}
           src={imageUrl || DEFAULT_IMAGE}
           alt={title}
-          width={320}
-          height={320}
+          width={300}
+          height={300}
           loading="lazy"
           quality={100}
         />
@@ -85,18 +87,19 @@ export const MinimalCard: React.FC<Props> = ({
         )}
         <p className="mt-3 text-lg font-medium leading-6">{title || <br />}</p>
 
-        <div className="flex my-2 space-x-3 align-bottom">
+        <div className="flex my-2 space-x-3 align-bottom ">
           <button
             type="button"
-            className=""
             title="Play/Pause"
             onClick={() => handleAlbumArtClick(previewUrl, imageUrl || DEFAULT_IMAGE)}
           >
             {renderIcon(isPlaying, imageUrl, nowPlayingImageUrl)}
           </button>
-          <a type="button" title="Download" href={constructUrl()}>
-            <FaDownload className={`${iconClassNames} h-[16px]`} />
-          </a>
+          {download && (
+            <a type="button" title="Download" href={constructUrl()}>
+              <FaDownload className={`${iconClassNames} h-[16px]`} />
+            </a>
+          )}
         </div>
 
         {artists && (
