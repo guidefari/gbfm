70125e5d50bfca4087ea6f270d6719c2c469cb8a
Guide Fari <guideg6@gmail.com>
2023-09-17 08:46:45 +0200

    stylish: tailwind line clamp

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -11,8 +11,8 @@ interface Props {
 }
 
 export const PostCard: React.FC<Props> = ({ date, title, description, thumbnailUrl, slug }) => (
-  <div className="grid pb-4 border-2 border-t-0 border-l-0 rounded-md shadow-lg sm:grid-flow-col sm:grid-cols-5 border-gb-tomato">
-    <CustomLink href={slug} as={slug} className="text-xl group sm:mr-3 sm:col-span-2">
+  <div className="grid px-1 pb-4 border-2 border-t-0 border-l-0 rounded-md shadow-lg sm:grid-flow-col sm:grid-cols-5 border-gb-tomato">
+    <CustomLink href={slug} as={slug} className="my-auto text-xl group sm:mr-3 sm:col-span-2">
       <Image
         className="object-cover w-32 transition ease-in-out rounded-md shadow-md sm:w-full sm:col-span-2 aspect-square group-hover:ring-4 ring-gb-highlight"
         src={
@@ -27,11 +27,13 @@ export const PostCard: React.FC<Props> = ({ date, title, description, thumbnailU
       />
     </CustomLink>
 
-    <p className="max-w-xs mx-1 mt-2 leading-normal sm:col-span-3 text-ellipsis bg-gb-bg">
+    <div className="sm:col-span-3">
       <CustomLink href={slug} as={slug} className="text-lg">
         {title}
       </CustomLink>
-      <span className="hidden sm:inline-block"> {description || ''}</span>
-    </p>
+      <p className="mt-2 text-xs leading-normal sm:text-base line-clamp-2 sm:line-clamp-3 bg-gb-bg">
+        {description || ''}
+      </p>
+    </div>
   </div>
 )
--- tailwind.config.js
+++ tailwind.config.js
@@ -36,5 +36,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/line-clamp')],
 }
