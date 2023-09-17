b4d7bcd055903a05e777f01b7dfd5a9534b847bc
Guide Fari <guideg6@gmail.com>
2023-09-17 08:17:36 +0200

    stylishh

--- src/pages/mixes/[slug].tsx
+++ src/pages/mixes/[slug].tsx
@@ -39,15 +39,16 @@ export default function PostPage({ mix }: { mix: Mix }) {
       />
 
       <div className="relative grid w-screen grid-flow-row mx-auto max-w-7xl md:grid-flow-col md:grid-cols-3 md:space-x-5">
-        <div className="md:pl-2 w-fit mx-auto md:max-w-[30%] md:fixed md:top-0 lg:max-w-full self-start md:col-span-1">
+        <div className="self-start px-2 mx-auto w-fit md:fixed md:max-w-[30%] md:top-0 md:col-span-1">
           <MinimalCard
             title={mix.title}
             previewUrl={mix.mp3Url}
             imageUrl={mix.thumbnailUrl ?? DEFAULT_IMAGE_URL}
+            download
           ></MinimalCard>
           <LilDate date={mix.date} />
         </div>
-        <article className="w-screen min-h-screen px-2 mt-6 prose break-words md:w-auto md:px-0 md:col-start-2 md:col-span-2 text-inherit prose-a:text-inherit hover:prose-a:text-gb-tomato lg:prose-xl">
+        <article className="min-h-screen px-2 mt-6 prose break-words md:w-auto md:px-0 md:col-start-2 md:col-span-2 text-inherit prose-a:text-inherit hover:prose-a:text-gb-tomato lg:prose-xl">
           {/* <p className=" md:my-8">
             Let's take this offline.{' '}
             <a href={mix.mp3Url} download={mix.title} rel="noreferrer">
