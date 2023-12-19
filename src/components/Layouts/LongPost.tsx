ec14b77c5ad1bfdbb07ef7483a1252fb7c5ada12
Guide Fari <guideg6@gmail.com>
2023-12-19 06:44:44 +0200

    refactor: curated using new component

--- src/components/Layouts/LongPost.tsx
+++ src/components/Layouts/LongPost.tsx
@@ -1,5 +1,62 @@
 import React from 'react'
+import { PageSEO } from '../SEO'
+import Image from 'next/image'
+import { LilDate } from '../common/LilDate'
+import { MDXcomponents } from '@/lib/mdx'
+import { useMDXComponent } from 'next-contentlayer/hooks'
 
-export const LongPost = () => {
-  return <div>LongPost</div>
+type Props = {
+  content: string
+  thumbnailUrl: string
+  title: string
+  date: string
+  description: string
+  canonicalUrl?: string
+}
+
+export const LongPost = ({
+  title,
+  thumbnailUrl,
+  description,
+  content,
+  canonicalUrl,
+  date,
+}: Props) => {
+  const MDXContent = useMDXComponent(content)
+  const encoded_title = encodeURIComponent(title)
+  const full_default_url = `https://goosebumps.fm/api/og?title=${encoded_title}`
+
+  return (
+    <>
+      <PageSEO
+        title={title}
+        description={description || 'Goosebumps.fm curated sounds'}
+        ogImageUrl={thumbnailUrl || full_default_url}
+        canonicalUrl={canonicalUrl || null}
+      />
+
+      <div className="relative grid grid-flow-row md:grid-flow-col md:grid-cols-3 md:space-x-5">
+        <div className="md:ml-2 mt-6 break-words rounded-md w-fit mx-auto md:max-w-[30%] md:fixed md:top-0  self-start md:col-span-1">
+          <Image
+            className="rounded-md "
+            src={
+              thumbnailUrl ||
+              'https://res.cloudinary.com/hokaspokas/image/upload/v1663215741/goosebumpsfm/generic_Thumb.svg'
+            }
+            alt={`Thumbnail image for post titled - ${title}`}
+            width={320}
+            height={320}
+            loading="lazy"
+            quality={100}
+          />
+          <h4 className="mx-2 text-left md:mx-0 text-gb-pastel-green-2">{title}</h4>
+          <LilDate date={date} />
+        </div>
+        <article className="min-h-screen px-2 mt-6 prose break-words md:w-auto md:px-0 md:col-start-2 md:col-span-2 text-inherit prose-a:text-inherit hover:prose-a:text-gb-tomato lg:prose-xl">
+          {description && <p className="text-left ">{description}</p>}
+          <MDXContent components={MDXcomponents} />
+        </article>
+      </div>
+    </>
+  )
 }
--- src/pages/curated/[slug].tsx
+++ src/pages/curated/[slug].tsx
@@ -1,9 +1,5 @@
-import { PageSEO } from 'src/components/SEO'
 import { allPosts, type Post } from '@/contentlayer/generated'
-import { useMDXComponent } from 'next-contentlayer/hooks' // eslint-disable-line
-import Image from 'next/image'
-import { MDXcomponents } from '../../lib/mdx'
-import { LilDate } from '@/components/common/LilDate'
+import { LongPost } from '@/components/Layouts/LongPost'
 
 export const getStaticPaths = () => {
   const paths: string[] = allPosts.map((post) => post.url)
@@ -15,7 +11,7 @@ export const getStaticPaths = () => {
 
 export const getStaticProps = ({ params }) => {
   const post: Post = allPosts.find(
-    (singlePost) => singlePost._raw.flattenedPath === `curated/${params.slug}` // eslint-disable-line
+    (singlePost) => singlePost._raw.flattenedPath === `curated/${params.slug}`
   )
 
   return {
@@ -26,41 +22,14 @@ export const getStaticProps = ({ params }) => {
 }
 
 export default function PostPage({ post }: { post: Post }) {
-  const MDXContent = useMDXComponent(post.body.code)
-
-  const encoded_title = encodeURIComponent(post.title)
-  const full_default_url = `https://goosebumps.fm/api/og?title=${encoded_title}`
   return (
-    <>
-      <PageSEO
-        title={post.title}
-        description={post.description || 'Goosebumps.fm curated sounds'}
-        ogImageUrl={post.thumbnailUrl || full_default_url}
-        canonicalUrl={post.canonicalUrl || null}
-      />
-
-      <div className="relative grid grid-flow-row md:grid-flow-col md:grid-cols-3 md:space-x-5">
-        <div className="md:ml-2 mt-6 break-words rounded-md w-fit mx-auto md:max-w-[30%] md:fixed md:top-0  self-start md:col-span-1">
-          <Image
-            className="rounded-md "
-            src={
-              post.thumbnailUrl ||
-              'https://res.cloudinary.com/hokaspokas/image/upload/v1663215741/goosebumpsfm/generic_Thumb.svg'
-            }
-            alt={`Thumbnail image for post titled - ${post.title}`}
-            width={320}
-            height={320}
-            loading="lazy"
-            quality={100}
-          />
-          <h4 className="mx-2 text-left md:mx-0 text-gb-pastel-green-2">{post.title}</h4>
-          <LilDate date={post.date} />
-        </div>
-        <article className="min-h-screen px-2 mt-6 prose break-words md:w-auto md:px-0 md:col-start-2 md:col-span-2 text-inherit prose-a:text-inherit hover:prose-a:text-gb-tomato lg:prose-xl">
-          {post.description && <p className="text-left ">{post.description}</p>}
-          <MDXContent components={MDXcomponents} />
-        </article>
-      </div>
-    </>
+    <LongPost
+      content={post.body.code}
+      title={post.title}
+      date={post.date}
+      thumbnailUrl={post.thumbnailUrl}
+      description={post.description}
+      canonicalUrl={post.canonicalUrl}
+    />
   )
 }
