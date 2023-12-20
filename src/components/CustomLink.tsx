6d21a02ab47ef57e3492c5811ce24de9de8f58a3
Guide Fari <guideg6@gmail.com>
2023-12-20 20:25:36 +0200

    styles

--- src/components/CustomLink.tsx
+++ src/components/CustomLink.tsx
@@ -2,11 +2,12 @@ import Link, { LinkProps } from 'next/link'
 
 type Props = {
   children: React.ReactNode
+  className?: string
 } & LinkProps
 
-export default function CustomLink({ as, href, children, ...otherProps }: Props) {
+export default function CustomLink({ as, href, children, className, ...otherProps }: Props) {
   return (
-    <Link as={as} href={href} {...otherProps}>
+    <Link as={as} href={href} className={className} {...otherProps}>
       {children}
     </Link>
   )
--- src/components/FrontPage/Content.jsx
+++ src/components/FrontPage/Content.jsx
@@ -18,42 +18,40 @@ const rgbDataURL = (r, g, b) =>
 
 export const Content = () => {
   return (
-    <section className="w-full px-4 py-16 mx-auto md:px-24 lg:px-8 lg:py-20">
-      <div className="grid gap-4 row-gap-5 sm:grid-cols-2 lg:grid-cols-3">
-        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
-          <div>
-            <h6 className="mb-2 font-semibold leading-5">
-              <CustomLink href="curated/how-to" as="curated/how-to">
-                👀 How to use this site
-              </CustomLink>
-            </h6>
-            <p className="mb-3 text-sm">
-              Firstly, some house keeping. This site has a few experimental components to it.
-            </p>
-          </div>
+    <section className="curated-posts">
+      <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+        <div>
+          <h6 className="mb-2 font-semibold leading-5">
+            <CustomLink href="curated/how-to" as="curated/how-to">
+              👀 How to use this site
+            </CustomLink>
+          </h6>
+          <p className="mb-3 text-sm">
+            Firstly, some house keeping. This site has a few experimental components to it.
+          </p>
         </div>
+      </div>
 
-        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
-          <div>
-            <h6 className="mb-2 font-semibold leading-5">What kind of content is on here?</h6>
-            <p className="mb-3 text-sm">
-              <CustomLink href="#mixes">Mixes</CustomLink>, tweet-like{' '}
-              <CustomLink href="micro">micro-posts</CustomLink>,{' '}
-              <CustomLink href="#words">full-length posts, playlists</CustomLink>, and{' '}
-              <CustomLink href="labels">record label round-ups</CustomLink>.
-            </p>
-          </div>
+      <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+        <div>
+          <h6 className="mb-2 font-semibold leading-5">What kind of content is on here?</h6>
+          <p className="mb-3 text-sm">
+            <CustomLink href="#mixes">Mixes</CustomLink>, tweet-like{' '}
+            <CustomLink href="micro">micro-posts</CustomLink>,{' '}
+            <CustomLink href="#words">full-length posts, playlists</CustomLink>, and{' '}
+            <CustomLink href="labels">record label round-ups</CustomLink>.
+          </p>
         </div>
+      </div>
 
-        <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
-          <div>
-            <h6 className="mb-2 font-semibold leading-5">Community effort</h6>
-            <p className="mb-3 text-sm">
-              The idea behind this site is to build it as a{' '}
-              <CustomLink href="collaborate">community effort</CustomLink>. UI/UX feedback, track
-              suggestions, guest mixes, illustrations, you name it.
-            </p>
-          </div>
+      <div className="flex flex-col justify-between p-5 border rounded shadow-sm">
+        <div>
+          <h6 className="mb-2 font-semibold leading-5">Community effort</h6>
+          <p className="mb-3 text-sm">
+            The idea behind this site is to build it as a{' '}
+            <CustomLink href="collaborate">community effort</CustomLink>. UI/UX feedback, track
+            suggestions, guest mixes, illustrations, you name it.
+          </p>
         </div>
       </div>
     </section>
