50f60c9de384a2266bf94ab7df78daccfebf82d7
Guide Fari <guideg6@gmail.com>
2024-01-27 00:10:16 +0200

    feat: copy to clipboard util

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,3 +1,11 @@
 export function cn(...classes: string[]) {
   return classes.filter(Boolean).join(' ')
 }
+
+export function copyToClipboard(text: string) {
+  try {
+    navigator.clipboard.writeText(text)
+  } catch (error) {
+    console.warn('Failed to copy text: ', error)
+  }
+}
