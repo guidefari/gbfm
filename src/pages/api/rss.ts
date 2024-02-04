5089e1ff63a745a0673c001c05e752b39e5cfca9
Guide Fari <guideg6@gmail.com>
2024-02-04 11:23:47 +0200

    debug

--- src/pages/api/rss.ts
+++ src/pages/api/rss.ts
@@ -1,45 +1,46 @@
 import siteMetadata from '@/src/siteMetadata';
 import { NextApiRequest, NextApiResponse } from 'next';
-import {allMixes} from'@/contentlayer/generated'
+import { allMixes } from '@/contentlayer/generated'
 import fetch from 'node-fetch';
 
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const DEFAULT_IMAGE_URL = 'https://res.cloudinary.com/hokaspokas/image/upload/v1663215495/goosebumpsfm/spotify_filler.png'
 
+  let count = 0
+
   try {
-      const mixesRSSified = await Promise.all(allMixes.map(async (mix) => {
-        const url = `https://${req.headers.host}${mix.url}`;
-        const {headers} = await fetch(mix.mp3Url)
-        const contentLength = headers.get('content-length')
-        
-        return `<item>
+    const mixesRSSified = await Promise.all(allMixes.map(async (mix) => {
+      const url = `https://${req.headers.host}${mix.url}`;
+      count++
+      const { headers } = await fetch(mix.mp3Url)
+      const contentLength = headers.get('content-length')
+
+      return `<item>
           <title>${mix.title}</title>
           <link>${url}</link>
           <guid>${mix.mp3Url}</guid>
           <enclosure url="${mix.mp3Url}" type="audio/mpeg" length="${contentLength}"/>
           <pubDate>${new Date(mix.date).toUTCString()}</pubDate>
-          ${
-            mix.description &&
-            `<description>${mix.description}
-            Get the tracklist and more a immersive experience over at ${url}
-            </description>
-          `
-          }
+          ${mix.description ?
+        `<description>${mix.description}. Get the tracklist and more a immersive experience over at ${url}</description>` : ''
+        }
           <itunes:image href="${mix.thumbnailUrl ?? DEFAULT_IMAGE_URL}"/>
           <itunes:subtitle>${mix.title}</itunes:subtitle>
           <itunes:summary>${mix.description}</itunes:summary>
-          ${ mix.genres && `
+          ${mix.genres ? `
             <itunes:keywords>${mix.genres.join(', ')}</itunes:keywords>
-          `}
+          `: ''}
           <itunes:author>Guide Fari</itunes:author>
           <dc:creator>Guide Fari</dc:creator>
-          <itunes:explicit>false</itunes:explicit>
+          <itunes:explicit>no</itunes:explicit>
           </item>`
-        }))
-        ;
-        
-        const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    }))
+      ;
+
+      console.log(mixesRSSified.join(''))
+    // console.log(count)
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
         <rss xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:atom="http://www.w3.org/2005/Atom" version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd">
         <channel>
         <title>${siteMetadata.title}</title>
@@ -50,8 +51,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         <url>${DEFAULT_IMAGE_URL}</url>
         <title>goosebumps.fm</title>
         <link>${DEFAULT_IMAGE_URL}</link>
-        <width>1440</width>
-        <height>1440</height>
+        <width>400</width>
+        <height>400</height>
         </image>
       <itunes:image href="${DEFAULT_IMAGE_URL}"/>
       <itunes:category text="Music"/>
@@ -66,6 +67,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
     return res.status(200).send(sitemap);
   } catch (e: unknown) {
+    console.log('e:', e)
     if (!(e instanceof Error)) {
       throw e;
     }
