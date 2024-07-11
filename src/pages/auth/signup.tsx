1f347b60d56e237c99fa325f493cfee61ae7a706
Guide Fari <guideg6@gmail.com>
2024-07-11 07:38:35 +0200

    init shadcn

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -29,16 +29,6 @@ function SignUpPage() {
 
 	return (
 		<div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
-			<div className="sm:mx-auto sm:w-full sm:max-w-sm">
-				<img
-					className="mx-auto h-10 w-auto"
-					src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-					alt="Your Company"
-				/>
-				<h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-					Create new account
-				</h2>
-			</div>
 			<div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 				<form className="space-y-6" onSubmit={onSubmit}>
 					<div>
@@ -72,7 +62,7 @@ function SignUpPage() {
 							<div className="text-sm">
 								<Link
 									href="/auth/forgot"
-									className="font-semibold text-indigo-6000 hover:text-indigo-500"
+									className="font-semibold text-indigo-600 hover:text-indigo-500"
 								>
 									Forgot password?
 								</Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/pages/**/*.{js,ts,jsx,tsx}', './src/components/**/*.{js,ts,jsx,tsx}'],
+  darkMode: ["class"],
+  content: [
+    './src/pages/**/*.{ts,tsx, jsx}',
+    './src/components/**/*.{ts,tsx, jsx}',
+    './src/app/**/*.{ts,tsx, jsx}',
+    './src/**/*.{ts,tsx, jsx}',
+  ],
   theme: {
     extend: {
+      container: {
+        center: true,
+        padding: "2rem",
+        screens: {
+          "2xl": "1400px",
+        },
+      },
       fontFamily: {
         jetbrains: [
           'JetBrainsMono',
@@ -25,9 +38,59 @@ module.exports = {
         'gb-tomato': 'var(--highlight)',
         'gb-pastel-green-1': 'var(--pastel-green-1)',
         'gb-pastel-green-2': 'var(--pastel-green-2)',
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: {
+          DEFAULT: "hsl(var(--primary))",
+          foreground: "hsl(var(--primary-foreground))",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
       },
       animation: {
         'spin-slow': 'spin 3s linear infinite',
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       scale: {
         101: '1.01',
@@ -37,5 +100,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography')],
+  plugins: [require('@tailwindcss/typography'), require("tailwindcss-animate")],
 }
