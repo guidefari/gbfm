52ed8ce5d8e0afaaf32699b185f1cf4d5e0b9ecb
Guide Fari <guideg6@gmail.com>
2024-08-03 06:29:18 +0200

    skill issue

--- src/pages/api/auth/update-profile.ts
+++ src/pages/api/auth/update-profile.ts
@@ -1,38 +1,39 @@
 import db from "@/src/data/pocketbase";
 import type {
-    PocketBaseSignUpResponse,
-    GoosebumpsUser,
+  PocketBaseSignUpResponse,
+  GoosebumpsUser,
 } from "@/src/types/auth";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 // export const runtime = "edge";
 
 export default async function handler(
-    request: NextApiRequest,
-    res: NextApiResponse<GoosebumpsUser | { error: string } | unknown>,
+  request: NextApiRequest,
+  res: NextApiResponse<GoosebumpsUser | { error: string } | unknown>,
 ) {
-    try {
-        const body = await JSON.parse(request.body);
-        console.log('body:', body)
-        if (!body.email || !body.password) {
-            return res.status(400).json({ error: "Email and password are required" });
-        }
-
-        const thingy = {
-
-
-        }
-
-        const result: PocketBaseSignUpResponse = await db.updateProfile({ username: body.username, id: body.id });
-        // const sanitisedUser: GoosebumpsUser = {
-        // 	id: result.result.id,
-        // 	email: body.email,
-        // 	username: result.result.username,
-        // 	avatarUrl: result.result.avatar,
-        // };
-
-        return res.status(200).json(result);
-    } catch (err) {
-        return res.status(500).json({ error: err.message || err.toString() });
+  try {
+    const body = await JSON.parse(request.body);
+    console.log('body:', body)
+    if (!body.email || !body.password) {
+      return res.status(400).json({ error: "Email and password are required" });
     }
+
+    const thingy = {
+
+
+    }
+
+    // as RPM would say, "DISGUSTING", lol. need runtime validation of these inputs
+    const result: PocketBaseSignUpResponse = await db.updateProfile({ username: body.username, id: body.id, email: body.email, avatarUrl: body.avatarUrl });
+    // const sanitisedUser: GoosebumpsUser = {
+    // 	id: result.result.id,
+    // 	email: body.email,
+    // 	username: result.result.username,
+    // 	avatarUrl: result.result.avatar,
+    // };
+
+    return res.status(200).json(result);
+  } catch (err) {
+    return res.status(500).json({ error: err.message || err.toString() });
+  }
 }
