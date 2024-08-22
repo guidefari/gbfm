f8ee407463ced4e5c715e0bacd1e1a76cc17213b
Guide Fari <guideg6@gmail.com>
2024-08-22 15:09:39 +0200

    wip

--- src/pages/api/auth/signup.ts
+++ src/pages/api/auth/signup.ts
@@ -1,4 +1,3 @@
-import db from "@/src/data/pocketbase";
 import type {
 	PocketBaseSignUpResponse,
 	GoosebumpsUser,
@@ -9,24 +8,26 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
 	request: NextApiRequest,
-	res: NextApiResponse<GoosebumpsUser | { error: string }>,
+	res: NextApiResponse,
 ) {
-	try {
-		const { body } = await request;
-		if (!body.email || !body.password) {
-			return res.status(400).json({ error: "Email and password are required" });
-		}
+	return res.status(200).json({success: true})
+	
+	// try {
+	// 	const { body } = await request;
+	// 	if (!body.email || !body.password) {
+	// 		return res.status(400).json({ error: "Email and password are required" });
+	// 	}
 
-		const result: PocketBaseSignUpResponse = await db.register(body.email, body.password);
-		const sanitisedUser: GoosebumpsUser = {
-			id: result.result.id,
-			email: body.email,
-			username: result.result.username,
-			avatarUrl: result.result.avatar,
-		};
+	// 	const result: PocketBaseSignUpResponse = await db.register(body.email, body.password);
+	// 	const sanitisedUser: GoosebumpsUser = {
+	// 		id: result.result.id,
+	// 		email: body.email,
+	// 		username: result.result.username,
+	// 		avatarUrl: result.result.avatar,
+	// 	};
 
-		return res.status(200).json(sanitisedUser);
-	} catch (err) {
-		return res.status(500).json({ error: err.message || err.toString() });
-	}
+	// 	return res.status(200).json(sanitisedUser);
+	// } catch (err) {
+	// 	return res.status(500).json({ error: err.message || err.toString() });
+	// }
 }
