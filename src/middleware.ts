0d23f5c23f7e26a3bb9da1712d7d22fad4f5404d
Guide Fari <guideg6@gmail.com>
2024-08-28 09:53:26 +0200

    fix: run middleware on api routes too
    - updated the matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,29 +1,46 @@
-import type { NextApiRequest, NextApiResponse } from "next"
-import { type NextRequest, NextResponse } from "next/server"
+import { verifyRequestOrigin } from "lucia";
+import type { NextApiRequest, NextApiResponse } from "next";
+import { type NextRequest, NextResponse } from "next/server";
 // import db from "./data/pocketbase";
 
-export const runtime = "experimental-edge"
+export const runtime = "experimental-edge";
+
+const protectedUrls = ["/settings", "/auth"];
 
 export async function middleware(request: NextRequest, res: NextApiResponse) {
-  console.log(`[middleware] ${request.method} ${request.url}`)
-  const pb_cookie = request.cookies.get("pb_auth")?.value
-  // const isLoggedIn = await db.isAuthenticated(pb_cookie);
-  const isLoggedIn = false
-  if (request.nextUrl.pathname?.startsWith("/auth")) {
-    if (isLoggedIn) {
-      return NextResponse.redirect(new URL("/", request.url))
-    }
-    return
-  }
+	CSRFCheck(request);
+	const session_cookie = request.cookies.get("auth_session")?.value;
+	const isLoggedIn = !!session_cookie;
 
-  if (!isLoggedIn) {
-    console.log("[middleware] bro please just login abeg")
-    // return NextResponse.redirect(new URL("/auth/login", request.url));
-  }
+	if (request.nextUrl.pathname?.startsWith("/auth")) {
+		if (isLoggedIn) {
+			return NextResponse.redirect(new URL("/", request.url));
+		}
+		return;
+	}
 
-  return NextResponse.next()
+	return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+	matcher: ["/((?!_next/static|_next/image|favicon.ico).*)"],
+};
+
+function CSRFCheck(request: NextRequest) {
+	if (request.method === "GET") {
+		return;
+	}
+
+	const originHeader = request.headers.get("origin");
+	// NOTE: You may need to use `X-Forwarded-Host` instead
+	const hostHeader = request.headers.get("Host");
+	if (
+		!originHeader ||
+		!hostHeader ||
+		!verifyRequestOrigin(originHeader, [hostHeader])
+	) {
+		return new NextResponse(null, {
+			status: 403,
+		});
+	}
 }
