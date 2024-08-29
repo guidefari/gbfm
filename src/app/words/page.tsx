1f1a259e3a30447d4b8b8c20c0bb899d67caa362
Guide Fari <guideg6@gmail.com>
2024-08-29 09:13:59 +0200

    chore: delete unnecessary abstractions

--- src/app/words/page.tsx
+++ src/app/words/page.tsx
@@ -1,6 +1,10 @@
-import { Words } from "@/components/FrontPage/Tabs/Words";
-import React from "react";
 import type { Metadata } from "next";
+import type { Post } from ".contentlayer/generated/types";
+import { PostCard } from "@/components/PostCard";
+import { PageTitle } from "@/components/common/PageTitle";
+import { allPosts } from "@/contentlayer/generated";
+import { DEFAULT_IMAGE_URL } from "@/constants";
+import { compareDesc } from "date-fns";
 
 export const metadata: Metadata = {
 	title: "Words",
@@ -8,5 +12,28 @@ export const metadata: Metadata = {
 };
 
 export default function WordsPage() {
-	return <Words />;
+	const posts: Post[] = allPosts
+		.sort((a, b) =>
+			compareDesc(new Date(a.lastmod || a.date), new Date(b.lastmod || b.date)),
+		)
+		.filter((post) => post.title !== "Template post");
+	const draftsFilteredOut = posts.filter((post) => post?.draft !== true);
+
+	return (
+		<section id="words">
+			<PageTitle title="Words" />
+			<div className="curated-posts">
+				{draftsFilteredOut.map((post: Post) => (
+					<PostCard
+						slug={post.url}
+						title={post.title}
+						description={post.description}
+						date={post.date}
+						key={post._id}
+						thumbnailUrl={post.thumbnailUrl ?? DEFAULT_IMAGE_URL}
+					/>
+				))}
+			</div>
+		</section>
+	);
 }
