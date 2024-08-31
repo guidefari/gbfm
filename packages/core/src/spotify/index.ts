a437112de76fc2947a81df9b769bbb0718d42557
Guide Fari <guideg6@gmail.com>
2024-08-31 22:42:06 +0200

    wip: spotify proxy

---
+++ packages/core/src/spotify/index.ts
@@ -0,0 +1,10 @@
+export namespace Spotify {
+	export interface Track {
+		id: string;
+		name: string;
+		artist: string;
+		album: string;
+		duration: number;
+		url: string;
+	}
+}
---
+++ packages/functions/src/api/spotify.ts
@@ -0,0 +1,4 @@
+import { OpenAPIHono, createRoute, z } from "@hono/zod-openapi";
+import { Result } from "./common";
+
+export namespace Spotify {}
