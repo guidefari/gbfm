e9d1acea3ddb165d18ae9c71a7dc62490ef08f12
Guide Fari <guideg6@gmail.com>
2024-09-14 19:09:29 +0200

    yeet components over

---
+++ packages/www/src/contexts/AuthContext.tsx
@@ -0,0 +1,85 @@
+"use client";
+import type React from "react";
+import { createContext, useContext, useEffect, useMemo, useState } from "react";
+import type { GoosebumpsUser, LoginResponse } from "../types/auth";
+import { readFromLocalStorage, writeToLocalStorage } from "@guide/utils";
+
+const AuthContext = createContext<IContext | null>(null);
+
+export function useAuthContext(): IContext {
+	const context = useContext(AuthContext);
+	if (context === null) {
+		throw new Error("useAuthContext must be used within an AuthProvider");
+	}
+	return context;
+}
+
+export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+	const [user, setUser] = useState<GoosebumpsUser | null>(null);
+
+	useEffect(() => {
+		const user = readFromLocalStorage<GoosebumpsUser>({
+			id: "user",
+			tableName: "goosebumps",
+		});
+		if (user) {
+			setUser(user);
+		}
+	}, []);
+
+	const onSignIn = (data: LoginResponse) => {
+		if ("error" in data) {
+			console.error(data.error);
+			return;
+		}
+		setUser({
+			email: data.email,
+			id: data.id,
+			username: data.username,
+		});
+		writeToLocalStorage({
+			id: "login_token",
+			tableName: "goosebumps",
+			data: data.token,
+		});
+		writeToLocalStorage({
+			id: "user",
+			tableName: "goosebumps",
+			data: data,
+		});
+	};
+	const onSignUp = (loggedInUser: GoosebumpsUser) => setUser(loggedInUser);
+	const onSignOut = () => {
+		setUser(null);
+		writeToLocalStorage({
+			id: "login_token",
+			tableName: "goosebumps",
+			data: null,
+		});
+		writeToLocalStorage({
+			id: "user",
+			tableName: "goosebumps",
+			data: null,
+		});
+	};
+
+	const contextValues: IContext = {
+		user,
+		onSignIn,
+		onSignUp,
+		onSignOut,
+	};
+
+	return (
+		<AuthContext.Provider value={contextValues}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
+
+type IContext = {
+	user: GoosebumpsUser | null;
+	onSignUp: (user: GoosebumpsUser) => void;
+	onSignIn: (data: LoginResponse) => void;
+	onSignOut: () => void;
+};
---
+++ packages/www/src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_IMAGE_URL =
+	"https://d20tmfka7s58bt.cloudfront.net/gb-default.png";
+export const CLOUDFRONT_IMAGES = "https://d20tmfka7s58bt.cloudfront.net";
