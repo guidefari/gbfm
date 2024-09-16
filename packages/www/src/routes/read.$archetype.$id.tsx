1afa48f9fe35f4598885d73dbcb697851334c5c8
Guide Fari <guideg6@gmail.com>
2024-09-16 23:56:42 +0200

    ux: loading state 👀

--- packages/www/src/routes/read.$archetype.$id.tsx
+++ packages/www/src/routes/read.$archetype.$id.tsx
@@ -1,21 +1,16 @@
 import { MDXRendrr } from "@/components/MDXRendrr";
 import { API_BASE_URL, fetcher } from "@/lib/http";
 import { useQuery } from "@tanstack/react-query";
-import { createFileRoute, useParams } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/read/$archetype/$id")({
 	component: ReadSingle,
-	// loader: async ({ params, location, route, context }) => {
-	// 	console.log({ params, location, route, context });
-	// 	const archetype = {};
-	// 	// return { archetype: params.archetype, id: params.id };
-	// },
 });
 
 function ReadSingle() {
 	const { archetype, id } = Route.useParams();
 
-	const { data, isLoading, error } = useQuery({
+	const { data, isLoading, error, isFetching } = useQuery({
 		queryKey: ["read-single"],
 		queryFn: async () =>
 			fetcher(`${API_BASE_URL}/mdx-archive/read`, {
@@ -26,7 +21,7 @@ function ReadSingle() {
 			}),
 	});
 
-	if (isLoading) return <div>Loading...</div>;
+	if (isLoading || isFetching) return <div>Loading...</div>;
 	if (error) return <div>Error: {error.message}</div>;
 
 	return <MDXRendrr mdxString={data?.compiled} />;
