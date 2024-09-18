3614b7383a2f8eb5f149782055614ab6741b1a91
Guide Fari <guideg6@gmail.com>
2024-09-18 06:46:07 +0200

    chore: copy styles over

--- packages/www/src/main.tsx
+++ packages/www/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
-import "./main.css";
+import "./styles/main.css";
 
 const router = createRouter({ routeTree });
 
