57ddcb9f36e235c3d8876d23a7419ea6a483a38f
Guide Fari <guideg6@gmail.com>
2024-10-05 18:39:57 +0200

    infra: link fn & auth

--- infra/api.ts
+++ infra/api.ts
@@ -1,13 +1,13 @@
-import { domain } from "./dns"
-import { allSecrets } from "./secret"
-import { isPermanentStage } from "./stage"
-import { bucket } from "./bucket"
+import { domain } from "./dns";
+import { allSecrets } from "./secret";
+import { isPermanentStage } from "./stage";
+import { bucket } from "./bucket";
 // import { bus } from "./events";
 // import { database } from "./database";
-import { email } from "./email"
-import { secret } from "./secret"
+import { email } from "./email";
+import { secret } from "./secret";
 
-if (!domain) throw new Error("no custom domain provided, what you doing blud?")
+if (!domain) throw new Error("no custom domain provided, what you doing blud?");
 
 // sst.Linkable.wrap(random.RandomString, (resource) => ({
 // 	properties: {
@@ -15,36 +15,37 @@ if (!domain) throw new Error("no custom domain provided, what you doing blud?")
 // 	},
 // }));
 
+export const auth = new sst.aws.Auth("Auth", {
+	authenticator: {
+		link: [email, secret.SquealDBUrl],
+		handler: "./packages/functions/src/auth.handler",
+		permissions: [
+			{
+				actions: ["ses:SendEmail"],
+				resources: ["*"],
+			},
+		],
+	},
+});
+
 const apiFn = new sst.aws.Function("OpenApi", {
-  handler: "./packages/functions/src/api/index.handler",
-  streaming: !$dev,
-  url: true,
-  link: [...allSecrets, bucket],
-})
+	handler: "./packages/functions/src/api/index.handler",
+	streaming: !$dev,
+	url: true,
+	link: [...allSecrets, bucket, auth],
+});
 
 export const api = new sst.cloudflare.Worker("OpenApiWorker", {
-  url: true,
-  // live: false,
-  domain: `openapi.${domain}`,
-  handler: "./packages/workers/src/proxy.ts",
-  environment: {
-    ORIGIN_URL: apiFn.url,
-    NO_CACHE: String(isPermanentStage),
-  },
-})
-
-export const auth = new sst.aws.Auth("Auth", {
-  authenticator: {
-    link: [email, secret.SquealDBUrl],
-    handler: "./packages/functions/src/auth.handler",
-    permissions: [
-      {
-        actions: ["ses:SendEmail"],
-        resources: ["*"],
-      },
-    ],
-  },
-})
+	url: true,
+	// live: false,
+	// link: [...allSecrets, bucket, auth],
+	domain: `openapi.${domain}`,
+	handler: "./packages/workers/src/proxy.ts",
+	environment: {
+		ORIGIN_URL: apiFn.url,
+		NO_CACHE: String(isPermanentStage),
+	},
+});
 
 // export const authRouter = new sst.aws.Router("AuthRouter", {
 //   domain: {
@@ -55,16 +56,16 @@ export const auth = new sst.aws.Auth("Auth", {
 // })
 
 export const authRouter = new sst.cloudflare.Worker("AuthWorkerCF", {
-  url: true,
-  dev: false,
-  domain: `auth.${domain}`,
-  handler: "./packages/workers/src/proxy.ts",
-  environment: {
-    ORIGIN_URL: auth.url,
-  },
-})
+	url: true,
+	dev: false,
+	domain: `auth.${domain}`,
+	handler: "./packages/workers/src/proxy.ts",
+	environment: {
+		ORIGIN_URL: auth.url,
+	},
+});
 
 export const outputs = {
-  auth: authRouter.url,
-  openapi: api.url,
-}
+	auth: authRouter.url,
+	openapi: api.url,
+};
