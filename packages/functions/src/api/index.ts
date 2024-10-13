3b13bc9b71914cc834361bb7ee56876db86abf88
Guide Fari <guideg6@gmail.com>
2024-10-13 08:58:06 +0200

    swag🤫

--- packages/functions/src/api/index.ts
+++ packages/functions/src/api/index.ts
@@ -1,15 +1,28 @@
 import { handle, streamHandle } from "hono/aws-lambda";
-import { OpenAPIHono } from "@hono/zod-openapi";
+import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
 import { SpotifyApi } from "./spotify";
 import { MDXArchiveApi } from "./mdx-archive";
+// import { AuthMiddleware } from "./auth.middleware";
+import { swaggerUI } from "@hono/swagger-ui";
 
 const app = new OpenAPIHono();
 
-app.get("/", (c) => {
-	return c.text("This space has been left intentionally blank🤫");
-});
+app.openapi(
+	createRoute({
+		method: "get",
+		path: "/",
+		responses: {
+			200: {
+				description: "Health check",
+			},
+		},
+	}),
+	(c) => c.json({ message: "sup, m8?" }, 200),
+);
 
 const routes = app
+	// TODO: auth on a per route basis. and none of these need auth
+	// .use("*", AuthMiddleware)
 	.route("/spotify", SpotifyApi.route)
 	.route("/mdx-archive", MDXArchiveApi.route);
 
@@ -20,6 +33,8 @@ app.doc("/doc", () => ({
 		version: "0.0.1",
 	},
 }));
+
+app.get("/swag", swaggerUI({ url: "/doc" }));
 
 export type Routes = typeof routes;
 export const handler = process.env.SST_LIVE ? handle(app) : streamHandle(app);
