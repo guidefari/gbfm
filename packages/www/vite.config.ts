00c1fe68b0161b989fe3837f6fe6f4048bedc84a
Guide Fari <guideg6@gmail.com>
2024-11-05 07:36:30 +0200

    ffs that vite config issue again

--- packages/www/vite.config.ts
+++ packages/www/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 //import react from "@vitejs/plugin-react-swc";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
+import { resolve } from "node:path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import mdx from "@mdx-js/rollup";
 
@@ -19,7 +19,7 @@ export default defineConfig({
 	],
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "./src"),
+			"@": resolve(__dirname, "./src"),
 		},
 	},
 });
