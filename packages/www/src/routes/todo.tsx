d1825c9bc3267ec8010482b861ede21a458d31c2
Guide Fari <guideg6@gmail.com>
2024-11-22 08:11:01 +0200

    todo

--- packages/www/src/routes/todo.tsx
+++ packages/www/src/routes/todo.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import todo from "@/mdx/todo.md";
+import todo from "@/mdx/todo.mdx";
 
 import { CustomMDXComponents } from "@/components/mdx-components";
 
