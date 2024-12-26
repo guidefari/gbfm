6e9625194511eef3e970b7b132e13c714a6aa1ea
Guide Fari <guideg6@gmail.com>
2024-12-26 14:36:37 +0200

    fix: remove `require` from tailwind config

--- packages/www/tailwind.config.js
+++ packages/www/tailwind.config.js
@@ -1,3 +1,6 @@
+import typography from "@tailwindcss/typography";
+import animate from "tailwindcss-animate";
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	darkMode: ["class"],
@@ -96,5 +99,5 @@ export default {
 			},
 		},
 	},
-	plugins: [require("@tailwindcss/typography"), require("tailwindcss-animate")],
+	plugins: [typography, animate],
 };
