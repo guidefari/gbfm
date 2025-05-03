ccdc1ecc49eb54c42ed14446a2e4b4bb7c8d7d64
Guide Fari <guideg6@gmail.com>
2025-05-03 16:39:25 +0200

    use api gateway instead of load balancer

--- infra/vps.ts
+++ infra/vps.ts
@@ -10,16 +10,19 @@ export const cluster = new sst.aws.Cluster("gbfm_cluster", {
 
 export const service = new sst.aws.Service("gbfm_vps", {
 	cluster,
-	loadBalancer: {
-		rules: [
-			{ listen: "80/http", redirect: "443/https" },
-			{ listen: "443/https", forward: "3000/http" },
-		],
-		domain: {
-			name: `vps.${domain}`,
-			dns: sst.cloudflare.dns(),
-		},
+	serviceRegistry: {
+		port: 3000,
 	},
+	// loadBalancer: {
+	// 	rules: [
+	// 		{ listen: "80/http", redirect: "443/https" },
+	// 		{ listen: "443/https", forward: "3000/http" },
+	// 	],
+	// 	domain: {
+	// 		name: `vps.${domain}`,
+	// 		dns: sst.cloudflare.dns(),
+	// 	},
+	// },
 	dev: {
 		directory: "./vps",
 		command: "bun dev",
@@ -29,6 +32,16 @@ export const service = new sst.aws.Service("gbfm_vps", {
 	},
 	// link: [bucket, fileRouter, etc],
 });
+
+const apiGateway = new sst.aws.ApiGatewayV2("gbfm_vps_gateway", {
+	vpc,
+	domain: {
+		name: `vps.${domain}`,
+		dns: sst.cloudflare.dns(),
+	},
+});
+
+apiGateway.routePrivate("$default", service.nodes.cloudmapService.arn);
 
 export const outputs = {
 	service: service.url,
