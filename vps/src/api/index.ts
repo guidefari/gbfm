4123b29969008cba3942e3df488aff36cdb157aa
Guide Fari <guideg6@gmail.com>
2025-05-04 16:36:47 +0200

    lol

--- vps/src/api/index.ts
+++ vps/src/api/index.ts
@@ -41,58 +41,58 @@ app.get("/health", async (c) => {
 	}
   });
 
-app.post("/reset-tables", async (c) => {
-	try {
-		const body = (await c.req.json()) as { tables?: string[] };
-		const tablesToReset = body?.tables;
+// app.post("/reset-tables", async (c) => {
+// 	try {
+// 		const body = (await c.req.json()) as { tables?: string[] };
+// 		const tablesToReset = body?.tables;
 
-		async function resetTable(table: Table) {
-			return await db.execute(
-				sql.raw(
-					`TRUNCATE TABLE ${getTableName(table)} RESTART IDENTITY CASCADE`,
-				),
-			);
-		}
+// 		async function resetTable(table: Table) {
+// 			return await db.execute(
+// 				sql.raw(
+// 					`TRUNCATE TABLE ${getTableName(table)} RESTART IDENTITY CASCADE`,
+// 				),
+// 			);
+// 		}
 
-    // todo: don't forget to add future tables. also, could probably do this declaratively tbh
-    // but that's for another day😝
-		const allTables = [
-			authorsTable,
-			postsTable,
-			publicationsTable,
+//     // todo: don't forget to add future tables. also, could probably do this declaratively tbh
+//     // but that's for another day😝
+// 		const allTables = [
+// 			authorsTable,
+// 			postsTable,
+// 			publicationsTable,
 			
-		];
+// 		];
 
 
-		if (tablesToReset?.length) {
-      const failedToReset = new Set<string>();
-      const successfullyReset = new Set<string>();
+// 		if (tablesToReset?.length) {
+//       const failedToReset = new Set<string>();
+//       const successfullyReset = new Set<string>();
 
-			for (const tableName of tablesToReset) {
-				const table = allTables.find((t) => getTableName(t) === tableName);
+// 			for (const tableName of tablesToReset) {
+// 				const table = allTables.find((t) => getTableName(t) === tableName);
         
-				if (!table) {
-					failedToReset.add(tableName);
-          continue;
-				}
-				await resetTable(table);
-        successfullyReset.add(tableName);
-			}
-			return c.json({
-        ok: Array.from(successfullyReset),
-        notOk: Array.from(failedToReset),
-			}, 201);
-		}
+// 				if (!table) {
+// 					failedToReset.add(tableName);
+//           continue;
+// 				}
+// 				await resetTable(table);
+//         successfullyReset.add(tableName);
+// 			}
+// 			return c.json({
+//         ok: Array.from(successfullyReset),
+//         notOk: Array.from(failedToReset),
+// 			}, 201);
+// 		}
 		
-		for (const table of allTables) {
-			await resetTable(table);
-		}
-		return c.json({ message: "Reset all tables successfully" });
-	} catch (error) {
-		console.error("Error resetting tables:", error);
-		return c.json({ error: "Failed to reset tables", stack: error }, 500);
-	}
-});
+// 		for (const table of allTables) {
+// 			await resetTable(table);
+// 		}
+// 		return c.json({ message: "Reset all tables successfully" });
+// 	} catch (error) {
+// 		console.error("Error resetting tables:", error);
+// 		return c.json({ error: "Failed to reset tables", stack: error }, 500);
+// 	}
+// });
 
 export default {
 	port: 3003,
