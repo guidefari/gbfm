1cdcd15f5d4f506485ad6ee62a330f5fefc280c4
Guide Fari <guideg6@gmail.com>
2025-06-15 15:49:20 +0200

    fix: ffpeg path for mac

--- vps/src/api/mix.tsx
+++ vps/src/api/mix.tsx
@@ -189,7 +189,11 @@ async function createAudioOrVideo(
 						files.outputPath,
 					];
 
-		const ffmpegProcess = spawn(ffmpeg as string, ffmpegArgs);
+					const ffmpegPath = process.platform === 'darwin' 
+  ? '/usr/local/bin/ffmpeg'  // macOS path
+  : ffmpeg as string;
+		
+		const ffmpegProcess = spawn(ffmpegPath as string, ffmpegArgs);
 
 		ffmpegProcess.on("close", (code) => {
 			if (code === 0) {
