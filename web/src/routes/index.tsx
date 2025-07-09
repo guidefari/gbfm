ecb6b05e46bd79f3de99268e2eec6a22993f079b
Guide Fari <guideg6@gmail.com>
2025-07-10 00:23:02 +0200

    feat: add version to landing page

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import { version } from "../../package.json";
 
 export const Route = createFileRoute("/")({
 	component: Index,
@@ -7,11 +8,14 @@ export const Route = createFileRoute("/")({
 function Index() {
 	return (
 		<div className="flex flex-col justify-center items-center px-1 leading-none h-dvh">
-			<h1 className="my-0 text-5xl font-bold text-right w-fit md:text-7xl">
-				goosebumps.
-				<br />
-				<span className="text-highlight">fm</span>
-			</h1>
+			<div className="inline-block w-fit">
+				<h1 className="my-0 text-5xl font-bold text-right w-fit md:text-7xl">
+					goosebumps.
+					<br />
+					<span className="text-highlight">fm</span>
+					<aside className="text-sm text-right opacity-60">v{version}</aside>
+				</h1>
+			</div>
 		</div>
 	);
 }
