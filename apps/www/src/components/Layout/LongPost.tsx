aeb46e847ed298a0fda603f4ff4127d1c2e75ae5
Guide Fari <guideg6@gmail.com>
2025-08-03 17:45:00 +0200

    fix: grid on single mix page was wonky

--- apps/www/src/components/Layout/LongPost.tsx
+++ apps/www/src/components/Layout/LongPost.tsx
@@ -1,67 +1,67 @@
-import { LilDate } from "../common/LilDate";
-import { MinimalCard } from "../common/MinimalCard";
-import { DEFAULT_IMAGE_URL } from "@/lib/constants";
-import { MDXRendrr } from "../MDXRendrr";
+import { DEFAULT_IMAGE_URL } from '@/lib/constants'
+import { LilDate } from '../common/LilDate'
+import { MinimalCard } from '../common/MinimalCard'
+import { MDXRendrr } from '../MDXRendrr'
 
 type Props = {
-	content: string;
-	thumbnailUrl: string;
-	title: string;
-	date?: string;
-	description?: string;
-	youtubeId?: string;
-	mp3Url?: string;
-};
+  content: string
+  thumbnailUrl: string
+  title: string
+  date?: string
+  description?: string
+  youtubeId?: string
+  mp3Url?: string
+}
 
 export const LongPost = ({
-	title,
-	thumbnailUrl,
-	description,
-	content,
-	date,
-	youtubeId,
-	mp3Url,
+  title,
+  thumbnailUrl,
+  description,
+  content,
+  date,
+  youtubeId,
+  mp3Url
 }: Props) => {
-	return (
-		<>
-			<div className="relative grid grid-flow-row md:grid-flow-col md:grid-cols-3 md:space-x-5">
-				<div className="md:ml-2 mt-6 break-words rounded-md w-fit px-2 md:mx-auto md:max-w-[30%] md:fixed md:top-0 md:min-h-screen self-start md:col-span-1">
-					{mp3Url ? (
-						<MinimalCard
-							title={title}
-							previewUrl={mp3Url}
-							imageUrl={thumbnailUrl ?? DEFAULT_IMAGE_URL}
-							download
-							hideTitle
-						/>
-					) : (
-						<img
-							className="rounded-md "
-							src={thumbnailUrl || DEFAULT_IMAGE_URL}
-							alt={`Thumbnail for post titled - ${title}`}
-							width={320}
-							height={320}
-							loading="lazy"
-						/>
-					)}
-					<h4 className="text-left md:mx-0 text-gb-pastel-green-2">{title}</h4>
-					{date && <LilDate date={date} />}
-					{youtubeId && (
-						<iframe
-							width="100%"
-							height="auto"
-							src={`https://www.youtube.com/embed/${youtubeId}`}
-							title="YouTube video player"
-							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-							allowFullScreen
-						/>
-					)}
-				</div>
-				<article className="min-h-screen px-2 mt-6 break-words md:w-auto md:px-0 md:col-start-2 md:col-span-2 ">
-					{description && <p className="text-left ">{description}</p>}
-					<MDXRendrr mdxString={content} />
-				</article>
-			</div>
-		</>
-	);
-};
+  return (
+    <>
+      <div className='relative grid grid-flow-row lg:grid-flow-col lg:grid-cols-[auto_1fr] lg:gap-5'>
+        <div className='px-2 mt-6 break-words rounded-md w-fit lg:mx-auto lg:sticky lg:top-6 lg:self-start lg:col-span-1'>
+          {mp3Url ? (
+            <MinimalCard
+              title={title}
+              previewUrl={mp3Url}
+              imageUrl={thumbnailUrl ?? DEFAULT_IMAGE_URL}
+              download
+              hideTitle
+            />
+          ) : (
+            <img
+              className='rounded-md'
+              src={thumbnailUrl || DEFAULT_IMAGE_URL}
+              alt={`Thumbnail for post titled - ${title}`}
+              width={320}
+              height={320}
+              loading='lazy'
+            />
+          )}
+          <h4 className='text-left lg:mx-0 text-gb-pastel-green-2'>{title}</h4>
+          {date && <LilDate date={date} />}
+          {youtubeId && (
+            <iframe
+              width='100%'
+              height='auto'
+              src={`https://www.youtube.com/embed/${youtubeId}`}
+              title='YouTube video player'
+              allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+              allowFullScreen
+            />
+          )}
+        </div>
+        <article className='px-2 mt-6 min-h-screen break-words lg:w-auto lg:px-0 lg:col-span-2'>
+          {description && <p className='text-left'>{description}</p>}
+          <MDXRendrr mdxString={content} />
+        </article>
+      </div>
+    </>
+  )
+}
