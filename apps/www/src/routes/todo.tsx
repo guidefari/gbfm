2ddecf393d01b97cb023d3381dd15b69c004af34
Guide Fari <guideg6@gmail.com>
2025-08-06 22:03:51 +0200

    chore: move todos to github issues

--- apps/www/src/routes/todo.tsx
+++
@@ -1,8 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-import todo from "@/mdx/todo.mdx";
-
-import { CustomMDXComponents } from "@/components/mdx-components";
-
-export const Route = createFileRoute("/todo")({
-	component: () => todo({ components: CustomMDXComponents }),
-});
