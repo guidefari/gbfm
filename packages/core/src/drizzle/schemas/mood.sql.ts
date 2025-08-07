308dc94dc25a4ec3af617c95f45155aa6e3289a1
Guide Fari <guideg6@gmail.com>
2025-08-07 21:56:09 +0200

    chore: delete old code

--- packages/core/src/drizzle/schemas/mood.sql.ts
+++
@@ -1,9 +0,0 @@
-import { pgTable, text } from "drizzle-orm/pg-core";
-
-export const moodTable = pgTable("moods", {
-	id: text().primaryKey(),
-	name: text(),
-});
-
-export type Mood = typeof moodTable.$inferSelect;
-export type NewMood = typeof moodTable.$inferInsert;
