7149bbf9895dd342d83506602ca228301d01dd03
Guide Fari <guideg6@gmail.com>
2026-05-12 12:11:00 +0200

    fix(vps): Removed the loopback hostname

--- apps/vps/src/index.ts
+++ apps/vps/src/index.ts
@@ -5,6 +5,5 @@ const { default: app } = await import('./app')
 export default {
   port: localVPSPort,
   fetch: app.fetch,
-  maxRequestBodySize: 1024 * 1024 * 1000, // 1GB
-  hostname: '127.0.0.1'
+  maxRequestBodySize: 1024 * 1024 * 1000 // 1GB
 }
