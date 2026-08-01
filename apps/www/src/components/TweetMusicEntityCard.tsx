3fc91c00987df61fb24699ae65c27558ad83f672
Guide Fari <guideg6@gmail.com>
2026-08-01 19:27:31 +0200

    fix: style

--- apps/www/src/components/TweetMusicEntityCard.tsx
+++ apps/www/src/components/TweetMusicEntityCard.tsx
@@ -149,7 +149,7 @@ export function TweetMusicEntityCard({ entityType, entityId }: Props) {
         ) : null}
 
         {verifiedLinks.length > 0 && (
-          <div className='-mx-4 mt-auto overflow-x-auto border-t border-border/40 px-4 py-2 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden'>
+          <div className='-mx-4 mt-auto overflow-x-auto border-t border-border/40 px-4 py-2 sm:mx-0 [scrollbar-width:none] [&::-webkit-scrollbar]:hidden'>
             <div className='w-max'>
               <StreamLinks links={verifiedLinks} />
             </div>
